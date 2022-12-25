32958c189dacd949994d7939649d3a071f93b48e
semuratakuya <kaitakusya3@gmail.com>
2022-12-25 15:57:59 +0900

    記事投稿作成画面

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['posts' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+}
+    
